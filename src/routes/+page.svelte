43e62a411839ac63bdaa0852350148ec064d07e8
Floyd Jones <floydtjones@gmail.com>
2025-03-20 17:13:50 -0500

    add mango

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,20 +33,25 @@
 				<img src="https://github.com/flooyd/things/raw/main/public/images/grid.png?raw=true" alt="screenshot of app">
 			</a>	
         </div>
-        <div  transition:fly={{duration: 1000, x: -1000}} class="project">
-            <a href="https://github.com/flooyd/solidtd">
-			<div>Tower Defense Map Editor - Solid</div>
-			<img src="https://i.imgur.com/xKox8dl.png" alt="screenshot of game">
-			</a>
-            
-	    </div>
-        <div  transition:fly={{duration: 1000, x: -1000}} class="project">
-            <a href="https://github.com/flooyd/dota2lfg">
-			<div>Dota 2 LFG - React and Express</div>
-			<img src="https://github.com/flooyd/dota2lfg/raw/master/readme%20images/dota2lfg.PNG" alt="screenshot of app">
+	<div  transition:fly={{duration: 1000, x: -1000}} class="project">
+            <a href="https://github.com/flooyd/mango">
+			<div>Mango - Dota 2 Replay Navigator</div>
+			<img src="https://raw.githubusercontent.com/flooyd/mango/refs/heads/master/client/public/Mango.PNG" alt="screenshot of app">
 			</a>
             
         </div>
+	<div  transition:fly={{duration: 1000, x: -1000}} class="project">
+            <a href="https://github.com/flooyd/dota2lfg">
+		<div>Dota 2 LFG - React and Express</div>
+		<img src="https://github.com/flooyd/dota2lfg/raw/master/readme%20images/dota2lfg.PNG" alt="screenshot of app">
+	    </a>
+        </div>
+        <div  transition:fly={{duration: 1000, x: -1000}} class="project">
+            <a href="https://github.com/flooyd/solidtd">
+		<div>Tower Defense Map Editor - Solid</div>
+		<img src="https://i.imgur.com/xKox8dl.png" alt="screenshot of game">
+		</a>
+	</div>
     </div>
 </main>
 {/if}
